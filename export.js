5fa2731cd1f2a22e87939869b8bad3796694bcfd
FuturoDevNatanael <139016675+FuturoDevNatanael@users.noreply.github.com>
2023-07-08 19:10:02 -0300

    Criando repositórios de projetos
    Esse projeto e um desafio do programa ONE, da Challenges Decodificador de Texto do curso java script, css, e html.

---
+++ export.js
@@ -0,0 +1,10 @@
+// Exporting variables
+export const cryptographButton = document.querySelector(".cryptograph-button")
+export const decryptionButton  = document.querySelector(".decryption-button")
+export const hint              = document.querySelector(".hint")
+
+export const textField       = document.querySelector("#text-field")
+export const showMessageArea = document.querySelector(".area-show-message")
+export const resultMessage   = document.querySelector("#result-message")
+export const copyButton      = document.querySelector("#copy-button")
+export const changeTheme      = document.querySelector("#change-theme")
---
+++ index.js
@@ -0,0 +1,202 @@
+import { 
+    cryptographButton, decryptionButton, hint,
+    textField, showMessageArea, resultMessage,
+    copyButton, changeTheme
+} from "./export.js";
+
+
+cryptographButton.addEventListener("click", () => {
+    
+    if(!textField.value || textField.value == " "){
+        
+        Swal.fire({
+            title      : "Nenhuma mensagem para criptografar",
+            text       : "Por favor, digite alguma mensagem com letras minúsculas, sem acentos e sem caracteres especiais.",
+            icon       : "error"
+        })
+    }
+    else{
+
+        let textToCheck = textField.value
+        let regexp = new RegExp(/[|!?@#/$%¨&+-.ºª;:*'"áéíóúãõâêîôûàèìòùäëïöüçÇA-Z0-9]+/g)
+
+        let checking = regexp.exec(textToCheck)
+
+        if(!checking){
+
+            cryptograph()
+            textField.style.background = ""
+        }
+        else{
+
+            Swal.fire({
+                title      : `Não foi possível criptografar`,
+                text       : `Remova o(s) seguinte(s) caractere(s): ${checking}`,
+                icon       : `error`
+            })
+
+            alertHint()
+        }
+    }
+})
+
+
+decryptionButton.addEventListener("click", () => {
+    
+    if(!textField.value || textField.value == " "){
+        Swal.fire({
+            title      : "Nenhuma mensagem para descriptografar",
+            text       : "Por favor, digite alguma mensagem com letras minúsculas, sem acentos e sem caracteres especiais.",
+            icon       : "error"
+        })
+    }
+    else{
+
+        let textToCheck = textField.value
+        let regexp = new RegExp(/[|!?@#/$%¨&+-.ºª;:*'"áéíóúãõâêîôûàèìòùäëïöüçÇA-Z0-9]+/g)
+
+        let checking = regexp.exec(textToCheck)
+
+        if(!checking){
+
+            decryption()
+            textField.style.background = ""
+        }
+        else{
+
+            Swal.fire({
+                title      : `Não foi possível descriptografar`,
+                text       : `Remova o(s) seguinte(s) caractere(s): ${checking}`,
+                icon       : `error`
+            })
+
+            alertHint()
+        }
+    }
+})
+
+
+changeTheme.addEventListener("click", () => {
+
+    let themeColor = document.body
+
+    themeColor.classList.toggle("dark-mode")
+    
+    changeTheme.className == "ph-fill ph-moon"? changeTheme.className = "ph-fill ph-sun" : changeTheme.className = "ph-fill ph-moon"
+})
+
+
+copyButton.addEventListener("click", () => {
+    
+    if(resultMessage){
+        copyText()
+    }
+    else{
+        Swal.fire({
+            title      : "Nenhuma mensagem para copiar",
+            text       : "Este campo está vazio",
+            icon       : "error"
+        })
+    }
+})
+
+
+function cryptograph(){
+
+    let getText = textField.value.split(" ")
+
+    for(let i = 0; i < getText.length; i++){
+
+        if(getText[i].includes("a") || getText[i].includes("e") || getText[i].includes("i") || getText[i].includes("o") || getText[i].includes("u")){
+            
+            showMessageArea.style.display = "none"
+            resultMessage.style.display   = "block"
+            copyButton.style.display      = "inline"    
+            
+            resultMessage.innerHTML += `${getText[i]
+                                            .replace(/e/g, "enter")
+                                            .replace(/i/g, "imes")
+                                            .replace(/a/g, "ai")
+                                            .replace(/o/g, "ober")
+                                            .replace(/u/g, "ufat")
+                                        } `
+            
+            cryptographButton.innerHTML = "Criptografado!"
+        }
+
+        setTimeout(() => { cryptographButton.innerHTML = "Criptografar" }, 3000)   
+    }
+
+    return
+}
+
+
+function decryption(){
+    let setText = textField.value.split(" ")
+    
+    for(let i = 0; i < setText.length; i++){
+
+        if(setText[i].includes("a") || setText[i].includes("e") || setText[i].includes("i") || setText[i].includes("o") || setText[i].includes("u")){
+            showMessageArea.style.display = "none"
+            resultMessage.style.display   = "block"
+            copyButton.style.display      = "inline"
+
+            resultMessage.innerHTML += `${setText[i]
+                                            .replace(/enter/g, "e")
+                                            .replace(/imes/g, "i")
+                                            .replace(/ai/g, "a")
+                                            .replace(/ober/g, "o")
+                                            .replace(/ufat/g, "u")
+                                        } `
+                            
+            decryptionButton.innerHTML = "Descriptografado!"
+        }
+
+        setTimeout(() => { decryptionButton.innerHTML = "Descriptografar" }, 3000)
+    }
+
+    return
+}
+
+
+function copyText(){
+    
+    let textToCopy = resultMessage.textContent
+    
+    if(textToCopy){
+
+        navigator.clipboard.writeText(textToCopy)
+        copyButton.innerHTML = "Copiado!"
+        resultMessage.innerHTML = ""
+
+        cryptographButton.addEventListener("click", () => {
+            copyButton.innerHTML = "Copiar"
+        })
+
+        decryptionButton.addEventListener("click", () => {
+            copyButton.innerHTML = "Copiar"
+        })
+    }
+    
+    return
+}
+
+
+function alertHint(){
+                
+    let alertHint = setInterval(() => {
+
+        hint.style.visibility = (hint.style.visibility == "hidden"? "" : "hidden")
+        
+    }, 500)
+
+    setTimeout(() => { 
+
+        clearInterval(alertHint) 
+        hint.style.visibility = ""
+
+    }, 6000) 
+    
+    return
+}
+
